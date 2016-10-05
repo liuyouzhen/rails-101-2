c3c2e33ad940a184cdfd1bc3352b1374d694a080
liuyouzhen <liuyouzhen@gmail.com>
2016-10-05 17:20:57 +0800

    implement is_member_of?(group)

--- db/migrate/20161005063705_create_group_relationships.rb
+++ db/migrate/20161005063705_create_group_relationships.rb
@@ -1,8 +1,7 @@
 class CreateGroupRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :group_relationships do |t|
-      t.string :group_id
-      t.integer :
+      t.integer :group_id
       t.integer :user_id
 
       t.timestamps
